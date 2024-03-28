000cf61957125ac86487cd2d0c1c1f1932fda1d1
Nicholas Marien <micro.ai.nick@gmail.com>
2024-03-27 20:57:22 -0400

    more

--- prices.js
+++ prices.js
@@ -4,6 +4,10 @@ d3.csv("prices.csv")
         .row(function(d){ return {month: parseDate(d.month), price:Number(d.price.trim().slice(1)) }; })
         .get(function(error,data){
 
-  console.log(data);
+          var height = 300;
+          var width = 500;
+
+          var max = d3.max(data, function(d){ return d.price; });
+          console.log(max)
 
 })
