afb0606e13b91991c8e8f707c86008b9e8879194
Nicholas Marien <micro.ai.nick@gmail.com>
2024-03-27 22:06:52 -0400

    Update dataHandlers.js

--- dataHandlers.js
+++ dataHandlers.js
@@ -12,6 +12,23 @@ d3.tsv("data.tsv")
     .row(function(d){ return {month: parseDate(d.month), price:Number(d.price.trim().slice(1))}; })
     .get(function(error,data){
 
-console.log(data);
+//console.log(data);
 
     });
+
+
+
+
+
+
+
+
+
+
+
+
+d3.json("treeData.json").get(function(error,data){
+
+      consol.log(data);
+
+});
